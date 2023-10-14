66d50ba78fecbe8fdd3ef0afd11b7920f0ae709e
Omar <omarsufi39@hotmail.com>
2023-10-14 04:18:26 -0700

    This is the initial commit for the workout app

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+const workoutRoutes = require('./routes/workouts');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+
+
+//Routes
+app.use('/api/workouts', workoutRoutes);
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to my site");
+})
+
+
+// Connect to db and listeing for requests
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(port);
+    console.log(`Database connected and server is listening on ${port}...`);
+}).catch((error) => console.log(error));
+
+
+
+
+
